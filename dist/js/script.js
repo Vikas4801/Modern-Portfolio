7bc43bbfac05ce9862ca3d98b0de1ba364411a87
vikas4801 <vikasrajput11304801@gmail.com>
2023-03-21 13:57:55 +0530

    menu button, javasript & rotate

---
+++ dist/js/script.js
@@ -0,0 +1,35 @@
+//Selcet DOM Items
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('close');
+        menuNav.classList.add('close');
+        menuBranding.classList.add('close');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //Set Menu State
+        showMenu = true;
+    } else {
+            menuBtn.classList.remove('close');
+            menu.classList.remove('close');
+            menuNav.classList.remove('close');
+            menuBranding.classList.remove('close');
+            navItems.forEach(item => item.classList.remove('show'));
+    
+            //Set Menu State
+            showMenu = false;
+    }
+}
