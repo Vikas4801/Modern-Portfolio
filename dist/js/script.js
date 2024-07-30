1a9c38e7b8bd67343de9978b34a357b5b0b94c5b
vikas4801 <vikasrajput11304801@gmail.com>
2024-07-30 12:58:54 +0530

    Initial commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,35 +1,42 @@
 //Selcet DOM Items
 
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const spanEl = document.querySelector("#main-footer span");
 
-const navItems = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll(".nav-item");
+
+let year = new Date().getFullYear();
+
+if (spanEl) {
+  spanEl.innerHTML = `<span>${year}</span>`;
+}
 
 //Set Initial State Of Menu
 let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
-        menuBtn.classList.add('close')
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
 
-        //Set Menu State
-        showMenu = true;
-    } else {
-            menuBtn.classList.remove('close');
-            menu.classList.remove('show');
-            menuNav.classList.remove('show');
-            menuBranding.classList.remove('show');
-            navItems.forEach(item => item.classList.remove('show'));
-    
-            //Set Menu State
-            showMenu = false;
-    }
+    //Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    //Set Menu State
+    showMenu = false;
+  }
 }
